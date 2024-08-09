11b9c6d7b8939628a857002de4cc08b1b372562d
Yagel <yagel19980@gmail.com>
2024-08-09 17:43:29 +0300

    start

---
+++ src/main/java/src/end_point/placementController.java
@@ -0,0 +1,14 @@
+package src.end_point;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/placements")
+public class placementController {
+
+
+
+
+}
